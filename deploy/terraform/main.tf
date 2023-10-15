48ce0f7eacafecaf3cd3c1e1370f2bc53c940c04
Your Name <you@example.com>
2023-10-15 15:46:24 +0000

    Add Terraform deployment sample

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,50 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "openex" {
+  count = 1
+  name       = "openex"
+  chart      = "./openex-charts"
+  depends_on = [ helm_release.minio ]
+
+  set {
+    name = "minio.enabled"
+    value = "false"
+  }
+  set {
+    name = "externalMinio.host"
+    value = "minio"
+  }
+  set {
+    name = "externalMinio.user"
+    value = "admin"
+  }
+  set {
+    name = "externalMinio.existingSecret"
+    value = "minio"
+  }
+
+  set {
+    name = "ingress.enabled"
+    value = "true"
+  }
+  set {
+    name = "ingress.hostname"
+    value = ""
+  }
+  set {
+    name = "ingress.tls"
+    value = "true"
+  }
+}
+
+resource "helm_release" "minio" {
+  name = "minio"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart = "minio"
+  
+}
+
